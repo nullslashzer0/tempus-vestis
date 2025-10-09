eff01de5da7d31f00d363dedc628ce813b79a024
Robert Wadsworth <robert.wadsworth414@gmail.com>
2025-10-09 15:43:16 -0400

    feat: add tools

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import os
 from dotenv import load_dotenv
 
 from core.constants import APP_NAME
+from tools.weather_api import get_weather_forecast
 
 load_dotenv()
 
 if __name__ == "__main__":
     print(f"Welcome to {APP_NAME} - an AI powered wardrobe planner")
+
+    print(get_weather_forecast(41.4553,-81.9179))
---
+++ src/tools/constants.py
@@ -0,0 +1,1 @@
+NWS_BASE_URL = "https://api.weather.gov"
